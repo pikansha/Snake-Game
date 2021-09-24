6e84f97c68f82f6eb1514c64f4b0b6e63203a228
Pikansha <56861522+pikansha@users.noreply.github.com>
2021-09-24 20:36:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -168,27 +168,3 @@ window.addEventListener('keydown', e => {
     }
 
 });
-function calculate(data) {
-    switch (data) {
-        case "up":
-            inputDir.x = 0;
-            inputDir.y = -1;
-            break;
-        case "down":
-            inputDir.x = 0;
-            inputDir.y = 1;
-            break;
-        case "right":
-            inputDir.x = 1;
-            inputDir.y = 0;
-
-            break;
-        case "left":
-            inputDir.x = -1;
-            inputDir.y = 0;
-            break;
-        default:
-
-    }
-}
-
